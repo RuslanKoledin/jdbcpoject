d87c6a7ac3845c12d813b1d4905058e916059b06
Ruslan <ruslan.effect@gmail.com>
2023-01-27 20:20:16 +0600

    commit

--- src/SqlJdbs2/jdbs2.sql
+++ src/SqlJdbs2/jdbs2.sql
@@ -28,12 +28,20 @@ values (1,'Бейти сарма','Шаклык в лаваше',1),
        (3,'Сендвич с лососем','хлеб,лосось,соус,овощи',5),
        (4,'Пицца Цили','фарш,острый соус , сыр',3),
        (5,'Босо лагма','Жаренный лагман',2);
+insert into food
+values (6,'Сан-торо','Острый суп',1);
 insert into orders
 values (1,2,5,'Руслан'),
        (2,1,1,'Арсен'),
        (3,5,3,'Bob'),
        (4,3,4,'Tom'),
        (5,3,4,'Sam');
+select * from cafes join food f on cafes.id = f.fk_cafesId;
+select * from orders where fk_cafesId = 3; -- заказы полученные в 'империя пиццы'
+select * from food order by title_of_food; -- цены на блюдо нет , сортировака по названию блюда;
+
+
+
 select * from cafes;
 select *from food;
 select *from orders;
